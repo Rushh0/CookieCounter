208dbc3d46c4d48f5cf544d00872c065fff2958c
Rushh0 <72839678+Rushh0@users.noreply.github.com>
2025-07-18 00:56:33 -0700

    Small Update - New Features and Styling
    Updates:
    -Added new font and styling to buttons
    -Added a reset feature
    -Added ability to click cookie image to increment counter

--- script.js
+++ script.js
@@ -1,16 +1,30 @@
 let num = 0
-
-let displayCount = document.getElementById("count-el")
-let saveCount = document.getElementById("save-btn")
+const displayCount = document.getElementById("count-el")
+const saveCount = document.getElementById("save-btn")
 
 function increment() {
-
     num = num + 1
-
     displayCount.innerText = num
-    
 }
 
 function save() {
-    saveCount.innerText = "Save Number: " + num 
+    saveCount.innerText = "Save Progress: " + num
+    localStorage.setItem("counter", num);
 }
+
+function reset() {
+    num = 0
+    saveCount.innerText = "Save Progress: " + num
+    displayCount.innerText = num
+    localStorage.setItem("counter", num);
+}
+
+function getLocalStorage() {
+    if (localStorage.getItem("counter") !== null) {
+        num = parseInt(localStorage.getItem("counter"))
+        saveCount.innerText = "Save Progress: " + num
+        displayCount.innerText = num
+    }
+}
+
+getLocalStorage();
